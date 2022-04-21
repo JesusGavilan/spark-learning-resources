d627a06b84c0ed12c61b115a05daa8ed86533734
JesusGavilan <jegavilgar@gmail.com>
2022-04-22 00:01:30 +0200

    Add data sources lesson

---
+++ spark-essentials/src/main/scala/part2dataframes/DataSources.scala
@@ -0,0 +1,147 @@
+package part2dataframes
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.types._
+import part2dataframes.DataFramesBasics.spark
+
+object DataSources extends App {
+  val spark = SparkSession
+    .builder()
+    .appName("Data Sources and Formats")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val carsSchema = StructType(
+    Array(
+      StructField("Name", StringType),
+      StructField("Miles_per_Gallon", DoubleType),
+      StructField("Cylinders", LongType),
+      StructField("Displacement", DoubleType),
+      StructField("Horsepower", LongType),
+      StructField("Weight_in_lbs", LongType),
+      StructField("Acceleration", DoubleType),
+      StructField("Year", DateType),
+      StructField("Origin", StringType)
+    ))
+
+  /*
+    Reading a DF:
+    - format
+    - schema (optional)
+    - zero or more options
+   */
+  val carsDF = spark.read
+    .format("json")
+    .schema(carsSchema) // enforce a schema
+    .option("mode", "failFast") // permissive (default)
+    .option("path", "src/main/resources/data/cars.json")
+    .load()
+
+// alternative reading with options map
+  val carsDFWithOptionMap = spark.read
+    .format("json")
+    .options(
+      Map(
+        "mode"        -> "failFast",
+        "path"        -> "src/main/resources/data/cars.json",
+        "inferSchema" -> "true"
+      ))
+    .load()
+
+  /*
+   Writing DFs
+   - format
+   - save mode = overwrite, append, ignore, errorIfExists
+   - path
+   - zero or more options
+   */
+  carsDF.write
+    .format("json")
+    .mode(SaveMode.Overwrite)
+    .option("path", "src/main/resources/data/cars_dupe.json")
+    .save()
+
+  // JSON flags
+  spark.read
+    .schema(carsSchema)
+    .option("dateFormat", "YYYY-MM-dd") // couple with schema
+    .option("allowSingleQuotes", "true")
+    .option("compression", "uncompressed") // bzip2, gzip, etc
+    .json("src/main/resources/data/cars.json")
+
+  // CSV flags
+  val stocksSchema = StructType(
+    Array(
+      StructField("symbol", StringType),
+      StructField("date", DateType),
+      StructField("price", DoubleType)
+    ))
+
+  spark.read
+    .format("csv")
+    .schema(stocksSchema)
+    .option("dateFormat", "MMM dd YYYY")
+    .option("header", "true")
+    .option("sep", ",")
+    .option("nullValue", "")
+    .csv("src/main/resources/data/stocks.csv")
+
+  // Parquet
+  carsDF.write
+    .mode(SaveMode.Overwrite)
+    .save("src/main/resources/data/cars.parquet")
+
+  // Text Files
+  spark.read.text("src/main/resources/data/sampleTextFile.txt").show()
+
+  // Reading from a remote DB
+  val employeesDF = spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", "public.employees")
+    .load()
+
+  employeesDF.show()
+
+  /**
+    * Exercise: read the movies DF, then write it as
+    * - tab-separated values file
+    * - snappy Parquet
+    * - table "public.movies" in Postgres DB
+    */
+  val moviesDF = spark.read
+    .format("json")
+    .options(
+      Map(
+        "mode"        -> "failFast",
+        "path"        -> "src/main/resources/data/movies.json",
+        "inferSchema" -> "true"
+      ))
+    .load()
+
+  // CSV
+  moviesDF.write
+    .format("csv")
+    .mode(SaveMode.Overwrite)
+    .option("header", "true")
+    .option("sep", "\t")
+    .save("src/main/resources/data/movies.csv")
+  // Parquet
+  moviesDF.write
+    .mode(SaveMode.Overwrite)
+    .option("compression", "snappy") //but default is snappy
+    .save("src/main/resources/data/movies.parquet")
+
+  // DB
+  moviesDF.write
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", "public.movies")
+    .save()
+}
