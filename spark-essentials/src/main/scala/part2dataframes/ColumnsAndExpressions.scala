41ff8b95510894fbfe08eef6badf12e620979133
JesusGavilan <jegavilgar@gmail.com>
2022-05-07 02:07:36 +0200

    Added columnsAndExpression update

--- spark-essentials/src/main/scala/part2dataframes/ColumnsAndExpressions.scala
+++ spark-essentials/src/main/scala/part2dataframes/ColumnsAndExpressions.scala
@@ -1,7 +1,7 @@
 package part2dataframes
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, column, expr}
+import org.apache.spark.sql.functions.{coalesce, col, column, expr, lit, sum}
 
 object ColumnsAndExpressions extends App {
 
@@ -40,4 +40,47 @@ object ColumnsAndExpressions extends App {
     weightInKgExpression.as("Weight_in_kg"),
     expr("Weight_in_lbs / 2.2").as("Weight_in_kg_2")
   )
+
+  /**
+    * Exercises
+    * 1. Read the movies DF and select 2 columns of your choice
+    2.Create another column up the total profit of the movies = US_GROSS + Worldwide_GROSS + US_DVD_Sales
+    3. Select all COMEDY with IMDB rating above 6
+    Use as many as possible
+    */
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  val selectMoviesDF1 = moviesDF.select(col("Title"), col("Director"))
+  selectMoviesDF1.show(false)
+
+  val selectMoviesDF2 = moviesDF.select('Title, 'Major_Genre)
+  selectMoviesDF2.show(false)
+
+  val selectMoviesDF3 = moviesDF.select(column("Title"), column("Release_Date"))
+  selectMoviesDF3.show(false)
+
+  val selectMoviesDF4 = moviesDF.selectExpr("Title", "IMDB_Votes")
+  selectMoviesDF4.show()
+  val moviesTotalCostDF1 = moviesDF
+    .withColumn(
+      "total_profit",
+      coalesce(col("US_Gross"), lit(0)) + coalesce(col("Worldwide_Gross"), lit(0)) + coalesce(
+        col("US_DVD_Sales"),
+        lit(0)))
+  moviesTotalCostDF1.select('Title, 'total_profit).show(false)
+
+  val moviesTotalCostDF2 = moviesDF
+    .selectExpr(
+      "Title",
+      "US_Gross + Worldwide_Gross as total_profit"
+    )
+  moviesTotalCostDF2.show(false)
+
+  val comedyDF1 = moviesDF
+    .filter(col("Major_Genre") === "Comedy" and col("IMDB_Rating") > 6.0)
+
+  comedyDF1.select('Title, 'Major_Genre, 'IMDB_Rating).show(false)
+
 }
